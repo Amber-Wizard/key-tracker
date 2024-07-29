cbf3cf8ad231aee92c378e62b4626bcb54cfdc83
Amber-Wizard <deathkill520@gmail.com>
2024-07-29 15:04:04 -0500

    Cache selenium driver

--- browser.py
+++ browser.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 import time
 import re
 # import matplotlib.pyplot as plt
