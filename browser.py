291abd18f4960766b977f4ec31bda1c76d30ee5a
Amber-Wizard <deathkill520@gmail.com>
2024-07-29 15:04:30 -0500

    Cache selenium driver

--- browser.py
+++ browser.py
@@ -3,6 +3,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.os_manager import ChromeType
 import time
 import re
 # import matplotlib.pyplot as plt
