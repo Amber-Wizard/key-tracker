ae8ea0b5b11baa2b3cf792af9be7ebefa6277844
Amber-Wizard <deathkill520@gmail.com>
2024-08-30 00:41:12 -0500

    Fix deck share links

--- pages/2_Deck_Analysis.py
+++ pages/2_Deck_Analysis.py
@@ -157,7 +157,7 @@ if 'deck_games' not in st.session_state:
 if 'deck_selection' not in st.session_state:
     deck_games = st.session_state.deck_games
     deck_link = st.session_state.deck_games['Deck Link'].iat[0]
-    wins = (deck_games['Winner'] == pilot).sum()
+    wins = (deck_games['Winner'] == st.session_state.pilot).sum()
     games = len(deck_games)
     losses = games - wins
     winrate = round(100 * wins / games)
