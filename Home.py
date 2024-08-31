5c44b18b829316ee7946e4a541b6245b88699983
Amber-Wizard <deathkill520@gmail.com>
2024-08-30 22:57:12 -0500

    Fix deck analysis

--- Home.py
+++ Home.py
@@ -182,6 +182,8 @@ else:
                     del st.session_state['deck_games']
                 if 'deck_data' in st.session_state:
                     del st.session_state['deck_data']
+                if 'share_id' in st.session_state:
+                    del st.session_state['share_id']
 
                 st.session_state.deck_selection = st.session_state.deck_log.iloc[selected_deck]
                 deck = st.session_state.deck_selection['Deck'].iat[0][0]
